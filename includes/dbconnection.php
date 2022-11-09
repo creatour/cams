78122780a2090d7804edc8663f2f49cd405bd1ee
creatour <artmiracle65@gmail.com>
2022-11-09 12:51:05 +0800

    Update dbconnection.php
    mao ni ako g edit admin arts

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,4 +1,10 @@
 <?php
+
+$server = "sql211.epizy.com";
+$username = "epiz_32958550";
+$password = "6qEuiokU5ZPjx";
+$dbname = "epiz_32958550_cams";
+
 $con=mysqli_connect("localhost", "root", "", "camsdb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
